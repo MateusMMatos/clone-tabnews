72a159c513f830e2f00d93cf7f3134b70a4677d1
Mateus Medeiros Matos <mateus.m.matos@hotmail.com>
2025-08-21 18:08:07 -0300

    adiciona `.prettierignore`

--- pages/index.js
+++ pages/index.js
@@ -2,13 +2,4 @@ function Home() {
   return <h1>Vai dar tudo certo! pode ficar tranquilo.</h1>;
 }
 
-function teste() {
-  console.log("teste");
-  return <h1>Teste</h1>;
-}
-
-function outroTeste() {
-  console.log("outroTeste");
-  return <h1>Outro Teste</h1>;
-}
 export default Home;
