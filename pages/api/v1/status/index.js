96e9367fe52a17a51305dc19fb5ffa488a416ee9
Mateus Medeiros Matos <mateus.m.matos@hotmail.com>
2025-09-10 15:19:20 -0300

    finish `/api/v1/status` endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,28 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 as sum;");
-  console.log(result.rows);
-  response.status(200).json({ chave: "valor" });
+  const updateAt = new Date().toISOString();
+  const serverVersion = await database.query("SHOW server_version;");
+  const maxConnections = await database.query("SHOW max_connections;");
+
+  /* Alterei esta consulta para usar query parametrizada, evitando SQL Injection
+  const OpenConnections = await database.query(
+    "SELECT COUNT(*) FROM pg_stat_activity WHERE datname = current_database();",
+  );
+  */
+
+  const databaseName = process.env.POSTGRES_DB;
+  const databaseOpenedConnectionsResult = await database.query({
+    text: "SELECT COUNT(*) FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName],
+  });
+
+  response.status(200).json({
+    update_at: updateAt,
+    server_version: serverVersion.rows[0].server_version,
+    max_connections: parseInt(maxConnections.rows[0].max_connections),
+    open_connections: parseInt(databaseOpenedConnectionsResult.rows[0].count),
+  });
 }
 
 export default status;
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,35 @@
-test("GET to /api/v1/status should return 200", async () => {
-  const response = await fetch("http://localhost:3000/api/v1/status");
-  expect(response.status).toBe(200);
+describe("GET /api/v1/status", () => {
+  let response, body;
+
+  beforeAll(async () => {
+    response = await fetch("http://localhost:3000/api/v1/status");
+    body = await response.json();
+  });
+
+  test("should return status 200", () => {
+    expect(response.status).toBe(200);
+  });
+
+  test("should return update_at as valid ISO date", async () => {
+    expect(body.update_at).toBeDefined();
+    const parsedUpdateAt = new Date(body.update_at).toISOString();
+    expect(body.update_at).toEqual(parsedUpdateAt);
+  });
+
+  test("should return the Max Connections", async () => {
+    expect(body.max_connections).toBeDefined();
+    expect(Number.isInteger(body.max_connections)).toBe(true);
+    expect(body.max_connections).toBeGreaterThanOrEqual(100);
+  });
+
+  test("should return the server version", async () => {
+    expect(body.server_version).toBeDefined();
+    expect(body.server_version).toBe("16.0");
+  });
+
+  test("should return the Open Connections", async () => {
+    expect(body.open_connections).toBeDefined();
+    expect(Number.isInteger(body.open_connections)).toBe(true);
+    expect(body.open_connections).toBe(1);
+  });
 });
